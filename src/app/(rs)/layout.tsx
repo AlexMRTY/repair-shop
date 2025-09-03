34c83a468ee5da432df51b52856b558cc7c892ac
AlexMRTY <asifrezai261@gmail.com>
2025-09-03 21:50:29 +0200

    feat: add customer and tickets pages, home background image, and header component
    - Added new pages for Customer and Tickets.
    - Created a Home page with a background image.
    - Implemented a layout component for consistent structure.
    - Introduced a Header component with navigation links and theme toggle.
    - Integrated theme provider for dark/light mode support.
    - Added utility classes for animations and background images in CSS.
    - Included dropdown menu for theme selection.
    - Updated main Home page content to reflect business information.

---
+++ src/app/(rs)/layout.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+
+export default async function RSLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div>
+      <Header />
+      <div>
+        {children}
+      </div>
+      {/* FOOTER */}
+    </div>
+  );
+}
